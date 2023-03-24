087728ac24dee3143d0973ccf358750a6e1eebf1
pod042 <stanly61315@gmail.com>
2023-03-24 11:26:48 +0800

    try lazygit

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,11 @@
 return {
-    'tpope/vim-fugitive',
-    cmd = "Git",
+    -- 'tpope/vim-fugitive',
+    -- cmd = "Git",
+    -- keys = {
+    --     { "<leader>gg", vim.cmd.Git }
+    -- }
+    'kdheepak/lazygit.nvim',
     keys = {
-        { "<leader>gg", vim.cmd.Git }
+        { "<leader>gg", vim.cmd.LazyGit }
     }
 }

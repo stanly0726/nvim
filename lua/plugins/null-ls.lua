8bce4fddce8b990da0b0364ee64c6be6d1a49352
pod042 <stanly61315@gmail.com>
2023-05-04 11:35:26 +0800

    feat: use clang_format with null_ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -11,6 +11,7 @@ return {
                 }),
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.prettierd,
+                null_ls.builtins.formatting.clang_format,
             },
             -- debug = true,
         }

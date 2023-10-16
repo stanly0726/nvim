ce61b85e65701ea0530a39cde7c2de48df0c8c23
wsl2 Ubuntu <stanly61315@gmail.com>
2023-10-16 20:04:34 +0800

    chore: disable lualine in nvim-tree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,17 @@
 return {
     'nvim-lualine/lualine.nvim',
-    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    -- event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    event = { 'VeryLazy' },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
         options = {
             icons_enabled = true,
             theme = 'nord',
+            disabled_filetypes = { 'NvimTree' },
+            ignore_focus = { 'NvimTree' },
         },
         sections = {
             lualine_c = { { 'filename', path = 1 } },
-        }
+        },
     }
 }

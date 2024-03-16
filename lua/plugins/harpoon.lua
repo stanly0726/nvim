f96ffe649c117e8cb7c37c59b637a9ce3fe34ed4
stanly@Tower <stanly61315@gmail.com>
2024-03-17 00:28:18 +0800

    chore: update harpoon to harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,17 +1,24 @@
 return {
     'theprimeagen/harpoon',
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon:setup()
+    end,
     keys = {
         { '<leader>a', function()
-            require("harpoon.mark").add_file()
+            require("harpoon"):list():append()
         end },
         { '<C-j>', function()
-            require("harpoon.ui").toggle_quick_menu()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
         end },
         { '<C-n>', function()
-            require("harpoon.ui").nav_file(1)
+            require("harpoon"):list():select(1)
         end },
         { '<C-e>', function()
-            require("harpoon.ui").nav_file(2)
+            require("harpoon"):list():select(2)
         end },
     }
 }

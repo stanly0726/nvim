284f4dc7b1058562058397f8191f29d66dbc6d8a
stanly <stanly61315@gmail.com>
2025-06-02 16:40:02 +0800

    feat: format on save

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -1,3 +1,5 @@
+local settings = require("core.settings")
+
 local autocmd = {}
 
 function autocmd.nvim_create_augroups(definitions)
@@ -36,12 +38,34 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
--- auto cd into pwd argument
-vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-        vim.print(vim.v.argv)
-    end,
-})
+-- format on save
+if settings["format_on_save"] then
+    vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+            if not client then
+                return
+            end
+
+            if client:supports_method('textDocument/formatting') then
+                vim.api.nvim_create_autocmd('BufWritePre', {
+                    buffer = args.buf,
+                    callback = function()
+                        vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                    end,
+                })
+            end
+        end,
+    })
+end
+
+-- -- auto cd into pwd argument
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--     callback = function()
+--         vim.print(vim.v.argv)
+--     end,
+-- })
 
 function autocmd.load_autocmds()
     local definitions = {

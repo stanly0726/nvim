63b7a7df7d17ec3a96e9aa71610bd6532d784bbe
stanly <stanly0726@proton.me>
2025-11-03 14:19:43 +0800

    chore: config cleanup

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     'nvim-lualine/lualine.nvim',
     -- event = { "BufReadPost", "BufAdd", "BufNewFile" },
     event = { 'VeryLazy' },
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
         options = {
             icons_enabled = true,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,8 @@ local settings = require("core.settings")
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        lazy = false,
         build = ':TSUpdate',
-        event = { "BufReadPost", "BufNewFile" },
         opts = {
             -- A list of parser names, or "all"
             ensure_installed = settings.treesitter_deps,
@@ -13,15 +13,7 @@ return {
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
+            highlight = { enable = true },
         },
         config = function(_, opts)
             require('nvim-treesitter.configs').setup(opts)

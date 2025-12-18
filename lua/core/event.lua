1e05fae78cec8388d43aa859fb504cb52a182445
stanly <stanly.chang@incrte.com>
2025-12-18 15:10:22 +0800

    chore: cleanup unused config

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -38,28 +38,6 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
--- format on save
--- if settings["format_on_save"] then
---     vim.api.nvim_create_autocmd('LspAttach', {
---         callback = function(args)
---             local client = vim.lsp.get_client_by_id(args.data.client_id)
--- 
---             if not client then
---                 return
---             end
--- 
---             if client:supports_method('textDocument/formatting') then
---                 vim.api.nvim_create_autocmd('BufWritePre', {
---                     buffer = args.buf,
---                     callback = function()
---                         vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
---                     end,
---                 })
---             end
---         end,
---     })
--- end
-
 -- -- auto cd into pwd argument
 -- vim.api.nvim_create_autocmd("VimEnter", {
 --     callback = function()

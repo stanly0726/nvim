6d8eee9eab240609b9ab8ef51e5351f45d5cd78b
stanly <stanly.chang@incrte.com>
2025-12-18 15:16:21 +0800

    feat(gitsign): add keymap

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,5 +3,10 @@ return {
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
     opts = {
         current_line_blame = true,
+    },
+    keys = {
+        { "<leader>gp", ":Gitsign preview_hunk<CR>", desc = "Gitsign: preview" },
+        { "]g",         ":Gitsign next_hunk<CR>",    desc = "Gitsign: next hunk" },
+        { "[g",         ":Gitsign prev_hunk<CR>",    desc = "Gitsign: previous hunk" },
     }
 }

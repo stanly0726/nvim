8d16a05bbd5c5d39d2cf90f967ea080a29fb88b8
stanly <stanly.chang@incrte.com>
2026-01-28 11:55:16 +0800

    feat(theme): switch to nvim version of everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -34,15 +34,16 @@ return {
         }
     },
     {
-        'sainnhe/everforest',
+        "neanias/everforest-nvim",
+        version = false,
         lazy = false,
-        priority = 1000,
-        config = function()
-            -- Optionally configure and load the colorscheme
-            -- directly inside the plugin declaration.
-            vim.g.everforest_enable_italic = true
-            vim.g.everforest_transparent_background = settings.transparent_background
-            vim.g.everforest_background = "medium"
-        end
+        priority = 1000, -- make sure to load this before all the other start plugins
+        -- Optional; default configuration will be used if setup isn't called.
+        main = "everforest",
+        opts = {
+            italics = true,
+            transparent_background_level = settings["transparent_background"] and 1 or 0,
+            background = "medium",
+        },
     }
 }

dc9ef8805bef61b8a113abc8bbb734a4b8f4b393
stanly <stanly.chang@incrte.com>
2026-01-28 11:54:46 +0800

    feat(gitsign): add stage_hunk keymap

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,6 +8,7 @@ return {
     },
     keys = {
         { "<leader>gp", ":Gitsign preview_hunk<CR>", desc = "Gitsign: preview" },
+        { "<leader>gs", ":Gitsign stage_hunk<CR>",   desc = "Gitsign: stage hunk",   mode = { 'n', 'x' } },
         { "]g",         ":Gitsign next_hunk<CR>",    desc = "Gitsign: next hunk" },
         { "[g",         ":Gitsign prev_hunk<CR>",    desc = "Gitsign: previous hunk" },
     }

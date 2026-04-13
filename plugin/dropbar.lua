f7e9b30bdfdfa48cf5a255e43b764ee371cadd91
stanly <stanly.chang@incrte.com>
2026-04-13 15:01:39 +0800

    feat(plugin): add dropbar

---
+++ plugin/dropbar.lua
@@ -0,0 +1,10 @@
+vim.pack.add({ 'https://github.com/Bekaboo/dropbar.nvim' })
+local dropbar_api = require('dropbar.api')
+vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+vim.keymap.set(
+  'n',
+  '[;',
+  dropbar_api.goto_context_start,
+  { desc = 'Go to start of current context' }
+)
+vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })

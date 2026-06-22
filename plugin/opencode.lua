c82618d7bed5846a7abe39937faef846e1944e8a
stanly <stanly.chang@incrte.com>
2026-06-22 10:52:18 +0800

    refactor: use toggleterm to manage opencode terminal

--- plugin/opencode.lua
+++ plugin/opencode.lua
@@ -6,21 +6,41 @@ vim.pack.add({
 })
 
 ---@type opencode.Opts
-vim.g.opencodedopts = {
+vim.g.opencode_opts = {
   -- Your configuration, if any; goto definition on the type or field for details
+  server = {
+    start = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local opencode = Terminal:new({
+        cmd = 'opencode --port',
+        id = 99,
+        display_name = 'opencode',
+        direction = 'vertical',
+      })
+      Terminal.open(opencode, 70, 'vertical')
+
+      function _opencode_toggle()
+        opencode:toggle()
+      end
+    end,
+  },
 }
 
 vim.o.autoread = true -- Required for `opts.events.reload`
 
 -- Recommended/example keymaps
 vim.keymap.set({ 'n' }, '<leader>o', function()
-  require('opencode').toggle()
+  if _opencode_toggle == nil then
+    require('opencode').start()
+  else
+    _opencode_toggle()
+  end
 end, { desc = 'Opencode: toggle UI' })
 vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
   require('opencode').select()
 end, { desc = 'Opencode: open menu' })
 vim.keymap.set({ 'x' }, '<leader>o', function()
-  require('opencode').ask('@this: ', { submit = true })
+  require('opencode').ask('@this: ')
 end, { desc = 'Opencode: ask selected' })
 
 vim.keymap.set({ 'n', 'x' }, 'go', function()

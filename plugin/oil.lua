d37355355b09a746b3b8fe217ccd2fd4789babc9
stanly <stanly.chang@incrte.com>
2026-06-22 10:51:50 +0800

    refactor: use oil instead of mini.files after startup

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -1,33 +1,33 @@
--- vim.pack.add({
---     'https://github.com/nvim-mini/mini.nvim',
---     'https://github.com/stevearc/oil.nvim'
--- })
---
--- require('mini.icons').setup()
--- require('oil').setup({
---     -- default_file_explorer = false,
---     delete_to_trash = true,
---     view_options = {
---         show_hidden = true,
---     },
---     float = {
---         max_width = 0.6,
---     },
---     keymaps = {
---         ["<C-s>"] = false,
---         ["<C-v>"] = { "actions.select", opts = { vertical = true } },
---         ["gd"] = {
---             desc = "Toggle file detail view",
---             callback = function()
---                 Oil_detail = not Oil_detail
---                 if Oil_detail then
---                     require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
---                 else
---                     require("oil").set_columns({ "icon" })
---                 end
---             end,
---         },
---     },
--- })
---
--- vim.keymap.set('n', '-', '<cmd>Oil --float<cr>', { desc = "Oil: open oil menu" })
+vim.pack.add({
+  'https://github.com/nvim-mini/mini.nvim',
+  'https://github.com/stevearc/oil.nvim',
+})
+
+require('mini.icons').setup()
+require('oil').setup({
+  default_file_explorer = false,
+  delete_to_trash = true,
+  view_options = {
+    show_hidden = true,
+  },
+  float = {
+    max_width = 0.6,
+  },
+  keymaps = {
+    ['<C-s>'] = false,
+    ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
+    ['gd'] = {
+      desc = 'Toggle file detail view',
+      callback = function()
+        Oil_detail = not Oil_detail
+        if Oil_detail then
+          require('oil').set_columns({ 'icon', 'permissions', 'size', 'mtime' })
+        else
+          require('oil').set_columns({ 'icon' })
+        end
+      end,
+    },
+  },
+})
+
+vim.keymap.set('n', '-', '<cmd>Oil --float<cr>', { desc = 'Oil: open oil menu' })

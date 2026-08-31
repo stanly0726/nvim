137b93c10b111fae9254fb0c4a72030b6680bd8c
stanly <stanly.chang@incrte.com>
2026-08-31 16:50:38 +0800

    chore: don't focus when opening snacks explorer

--- plugin/00-snacks.lua
+++ plugin/00-snacks.lua
@@ -39,22 +39,32 @@ vim.schedule(function()
     ['n|<leader>fr'] = bind.map_lua(Snacks.picker.registers):with_desc('Snacks: register picker'),
     ['n|<leader>sh'] = bind.map_lua(Snacks.picker.help):with_desc('Snacks: help picker'),
     ['n|<leader>sk'] = bind.map_lua(Snacks.picker.keymaps):with_desc('Snacks: keymaps picker'),
-    ['n|<leader>sw'] = bind.map_lua(function()
-      Snacks.picker.grep({ hidden = true })
-    end):with_desc('Snacks: workspace grep'),
-    ['n|<leader>sW'] = bind.map_lua(function()
-      Snacks.picker.grep_word({ hidden = true })
-    end):with_desc('Snacks: grep under cursor'),
-    ['n|<leader>ss'] = bind.map_lua(function()
-      Snacks.picker()
-    end):with_desc('Snacks: picker menu'),
-    ['n|<leader>sd'] = bind.map_lua(Snacks.picker.diagnostics):with_desc('Snacks: diagnostics picker'),
-    ['n|<leader>e'] = bind.map_lua(function()
-      Snacks.explorer()
-    end):with_desc('Snacks: explorer'),
+    ['n|<leader>sw'] = bind
+      .map_lua(function()
+        Snacks.picker.grep({ hidden = true })
+      end)
+      :with_desc('Snacks: workspace grep'),
+    ['n|<leader>sW'] = bind
+      .map_lua(function()
+        Snacks.picker.grep_word({ hidden = true })
+      end)
+      :with_desc('Snacks: grep under cursor'),
+    ['n|<leader>ss'] = bind
+      .map_lua(function()
+        Snacks.picker()
+      end)
+      :with_desc('Snacks: picker menu'),
+    ['n|<leader>sd'] = bind
+      .map_lua(Snacks.picker.diagnostics)
+      :with_desc('Snacks: diagnostics picker'),
+    ['n|<leader>e'] = bind
+      .map_lua(function()
+        Snacks.explorer({ enter = false })
+      end)
+      :with_desc('Snacks: explorer'),
   })
 
--- toggle Snacks.nvim zen mode
+  -- toggle Snacks.nvim zen mode
   vim.api.nvim_create_user_command('Zen', Snacks.zen.zen, {
     desc = 'toggle Snacks.nvim zen mode',
   })
